c763d398d5b150328ed40148aec593c0d4a7b56d
Kuda Mushore <kudakwashe.mushore@partner.bmw.co.za>
2024-09-03 13:00:16 +0200

    try

--- UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b20944e1d9c9a2dd5cf31f0946409b9dfcc5e02e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1f0e329edcb2bb4d62fcde95fa7f63de1c74ce6")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
